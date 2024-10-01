3a3233ccf1e2fd0e97c5689cf0afb9f8ef6b3f06
Theredhawksings <myeoseul@gmail.com>
2024-10-01 23:08:30 +0900

    업데이트

--- Random_hand_position_tracking.py
+++ Random_hand_position_tracking.py
@@ -1,26 +1,66 @@
-from pico2d import *
+from operator import truediv
 
+from pico2d import *
+import random
 
 open_canvas()
 ground = load_image('TUK_GROUND.png')
 character = load_image('run_animation.png')
-had = load_image('hand_arrow.png')
+hand = load_image('hand_arrow.png')
+
+hx = 0
+hy = 0
+
+def handupdate():
+    global hx, hy
+    hx = random.randrange(100, 700)
+    hy = random.randrange(100, 500)
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
 
 x=400
 y=300
 frame = 0
+running = True
 
-while True:
+while running:
+
     clear_canvas()
     ground.draw(400, 300)
 
-    x +=0
-    y +=0
+    handupdate()
 
-    frame = (frame + 1) % 4
+    a = (hy - y) / (hx - x)
+    b = y - x * a
 
-    character.clip_draw(frame*64, 128, 64, 64, x, y, 128, 128)
+    if (x >= hx):
+        for x in range(x, hx + 1, -1):
+            clear_canvas()
+            ground.draw(400, 300)
+            hand.clip_draw(0, 0, 50, 52, hx, hy, 100, 104)
+            y = a * x + b
+            character.clip_draw(frame * 64, 128, 64, 64, x, y, 128, 128)
+            update_canvas()
+            frame = (frame + 1) % 4
+            delay(0.01)
 
-    update_canvas()
-    #handle_events()
-    delay(0.05)
+    elif (x < hx):
+        for x in range(x, hx + 1, 1):
+            clear_canvas()
+            ground.draw(400, 300)
+            hand.clip_draw(0, 0, 50, 52, hx, hy, 100, 104)
+            y = a * x + b
+            character.clip_draw(frame * 64, 64, 64, 64, x, y, 128, 128)
+            update_canvas()
+            frame = (frame + 1) % 4
+            delay(0.01)
+
+    handle_events()
+    delay(0.1)
